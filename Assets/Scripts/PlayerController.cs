26d8e5e41696d5b9b7c3c5e7eef543722e7f2688
Maxim <maximbekhterev@gmail.com>
2018-03-29 18:31:40 +0700

    second

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public int money;
+	public int healPoints;
+	public int hungry;
+
+	public Text moneyText;
+	public GameObject healPointsPanel;
+	float healPointsPanelBaseWidth;
+
+	void Awake() {
+		AddMoney (0);
+	}
+
+	public void AddMoney(int amount) {
+		money += amount;
+		if (money < 0)
+			money = 0;
+
+		moneyText.text = money.ToString ();
+	}
+
+	public void PlayerDamage(int amount) {
+		if (amount > 0) {
+			healPoints -= amount;
+			if (healPoints <= 0) {
+				// TODO player is dead
+			}
+		}
+	}
+
+	public void PlayerHeal(int amount) {
+		if (amount > 0) {
+			healPoints += amount;
+			if (healPoints > 100) {
+				healPoints = 100;
+			}
+		}
+	}
+}
